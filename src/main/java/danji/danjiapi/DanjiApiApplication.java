34c84e6d463cb5b3e490b9663638fc33e13c5ac1
mingdodev <mingdo.dev@gmail.com>
2025-06-27 15:39:46 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/danji/danjiapi/DanjiApiApplication.java
@@ -0,0 +1,13 @@
+package danji.danjiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanjiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanjiApiApplication.class, args);
+    }
+
+}
