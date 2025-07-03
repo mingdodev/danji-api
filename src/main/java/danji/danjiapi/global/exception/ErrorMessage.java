3b0716ecc53c30a2c746a74845e60fa60d5e1090
mingdodev <mingdo.dev@gmail.com>
2025-07-03 22:56:39 +0900

    feat: 인증 관련 에러 메시지 추가

--- src/main/java/danji/danjiapi/global/exception/ErrorMessage.java
+++ src/main/java/danji/danjiapi/global/exception/ErrorMessage.java
@@ -1,24 +1,32 @@
 package danji.danjiapi.global.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@AllArgsConstructor
 public enum ErrorMessage {
+
+    /**
+     * code: 사용자 정의 에러 코드
+     * httpStatus: 예외 상황에 알맞은 http 에러 코드
+     * message: 에러 메시지
+     */
+
+    // global
     INVALID_INPUT("INVALID_INPUT", "잘못된 입력입니다.", HttpStatus.BAD_REQUEST),
     RESOURCE_NOT_FOUND("RESOURCE_NOT_FOUND", "요청한 리소스를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     UNAUTHORIZED("UNAUTHORIZED", "인증되지 않은 요청입니다.", HttpStatus.UNAUTHORIZED),
     FORBIDDEN("FORBIDDEN", "요청 권한이 없습니다.", HttpStatus.FORBIDDEN),
     CONFLICT("CONFLICT", "리소스 충돌이 발생하였습니다.", HttpStatus.CONFLICT),
-    INTERNAL_ERROR("INTERNAL_ERROR", "알 수 없는 서버 내부 오류가 발생하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    INTERNAL_ERROR("INTERNAL_ERROR", "알 수 없는 서버 내부 오류가 발생하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 
+    // auth
+    AUTH_FORBIDDEN("AUTH_FORBIDDEN", "권한 정보가 없는 토큰입니다.", HttpStatus.FORBIDDEN),
+    AUTH_USER_NOT_FOUND("AUTH_USER_NOT_FOUND", "해당 인증 정보의 회원을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+;
     private final String code;
     private final String message;
     private final HttpStatus httpStatus;
-
-    ErrorMessage(String code, String message, HttpStatus httpStatus) {
-        this.code = code;
-        this.message = message;
-        this.httpStatus = httpStatus;
-    }
 }
