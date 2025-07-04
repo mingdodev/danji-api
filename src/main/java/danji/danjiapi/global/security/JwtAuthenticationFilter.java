85d1c60b3bb3a6feffbce738aa333f897d384c89
mingdodev <mingdo.dev@gmail.com>
2025-07-05 03:02:15 +0900

    refactor: JWT 토큰 검증 시 예외 처리
    - SecurityContext도 비워주기
    
    #1

--- src/main/java/danji/danjiapi/global/security/JwtAuthenticationFilter.java
+++ src/main/java/danji/danjiapi/global/security/JwtAuthenticationFilter.java
@@ -7,11 +7,13 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
     private final JwtTokenProvider jwtTokenProvider;
@@ -21,9 +23,19 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
             throws IOException, ServletException {
         String token = resolveToken((HttpServletRequest) servletRequest);
 
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (token != null) {
+            try {
+                if (jwtTokenProvider.validateToken(token)) {
+                    Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } else {
+                    log.debug("유효하지 않은 토큰입니다.");
+                    SecurityContextHolder.clearContext();
+                }
+            } catch (Exception e) {
+                log.error("JWT 인증 처리 중 예외 발생: {}", e.getMessage());
+                SecurityContextHolder.clearContext();
+            }
         }
         filterChain.doFilter(servletRequest, servletResponse);
     }
