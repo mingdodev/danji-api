cb47d40079eb11eae2404f6c3a61f6ca8fd6b7f2
mingdodev <mingdo.dev@gmail.com>
2025-07-06 00:58:46 +0900

    feat: JWT Authorization을 위한 Swagger 설정 추가
    #4

--- src/main/java/danji/danjiapi/domain/user/controller/UserController.java
+++ src/main/java/danji/danjiapi/domain/user/controller/UserController.java
@@ -7,6 +7,7 @@ import danji.danjiapi.domain.user.dto.response.UserCreateCustomerResponse;
 import danji.danjiapi.domain.user.service.UserService;
 import danji.danjiapi.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,13 +22,15 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup/customer")
-    @Operation(summary = "일반 회원 가입", description = "일반 회원의 회원 가입을 진행합니다.")
+    @Operation(summary = "일반 회원 가입", description = "일반 회원의 회원 가입을 진행합니다.",
+            security = @SecurityRequirement(name = ""))
     public ApiResponse<UserCreateCustomerResponse> signupCustomer(@Valid @RequestBody UserCreateCustomerRequest request) {
         return ApiResponse.success(userService.signupCustomer(request));
     }
 
     @PostMapping("/signup/merchant")
-    @Operation(summary = "사장님 회원 가입", description = "사장님의 회원 가입을 진행합니다. 회원 가입과 가게 생성이 하나의 프로세스로 진행됩니다.")
+    @Operation(summary = "사장님 회원 가입", description = "사장님의 회원 가입을 진행합니다. 회원 가입과 가게 생성이 하나의 프로세스로 진행됩니다.",
+            security = @SecurityRequirement(name = ""))
     public ApiResponse<UserCreateMerchantResponse> signupMerchant(@Valid @RequestBody UserCreateMerchantRequest request) {
         return ApiResponse.success(userService.signupMerchant(request));
     }
