ad3c29b8a6744ae9547f100f478068fd12ff63b0
mingdodev <mingdo.dev@gmail.com>
2025-07-06 17:08:18 +0900

    refactor: request DTO Valid 메시지 형식 통일
    #6

--- src/main/java/danji/danjiapi/domain/user/dto/request/UserCreateCustomerRequest.java
+++ src/main/java/danji/danjiapi/domain/user/dto/request/UserCreateCustomerRequest.java
@@ -7,9 +7,9 @@ public record UserCreateCustomerRequest(
         @NotBlank(message = "이메일은 필수입니다.")
         @Email(message = "올바른 이메일 형식이 아닙니다.")
         String email,
-        @NotBlank(message = "비밀번호는 필수입니다")
+        @NotBlank(message = "비밀번호는 필수입니다.")
         String password,
-        @NotBlank(message = "이름은 필수입니다")
+        @NotBlank(message = "이름은 필수입니다.")
         String name
 ) {
 }
