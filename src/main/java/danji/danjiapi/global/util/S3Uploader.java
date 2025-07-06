015eed189c2c849b254ed32705005574b141e3d1
mingdodev <mingdo.dev@gmail.com>
2025-07-07 04:58:14 +0900

    feat: S3 파일 업로드 유틸 구현
    - 환경 변수 설정
    
    #10

---
+++ src/main/java/danji/danjiapi/global/util/S3Uploader.java
@@ -0,0 +1,45 @@
+package danji.danjiapi.global.util;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import danji.danjiapi.global.exception.CustomException;
+import danji.danjiapi.global.exception.ErrorMessage;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+@RequiredArgsConstructor
+public class S3Uploader {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${aws.cloudfront.domain}")
+    private String cloudFrontDomain;
+
+    public String upload(MultipartFile file, String dirName) {
+        String filename = dirName + "/" + UUID.randomUUID() + "/" + file.getOriginalFilename();
+
+        try {
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+
+            amazonS3.putObject(new PutObjectRequest(bucket, filename, file.getInputStream(), metadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+
+            return "https://" + cloudFrontDomain + "/" + filename;
+
+        } catch (IOException e) {
+            throw new CustomException(ErrorMessage.USER_IMAGE_UPLOAD_FAILED);
+        }
+    }
+}
