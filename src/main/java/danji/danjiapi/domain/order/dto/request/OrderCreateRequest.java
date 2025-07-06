fab4419d4d52ffdd7e7c625837a8d09b5b262bae
mingdodev <mingdo.dev@gmail.com>
2025-07-07 03:14:37 +0900

    feat: 주문 요청 필드에 대한 검증 추가
    #8

--- src/main/java/danji/danjiapi/domain/order/dto/request/OrderCreateRequest.java
+++ src/main/java/danji/danjiapi/domain/order/dto/request/OrderCreateRequest.java
@@ -1,5 +1,6 @@
 package danji.danjiapi.domain.order.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,6 +11,8 @@ public record OrderCreateRequest(
         @NotNull(message = "배송 일자가 누락되었습니다.")
         LocalDateTime date,
         String deliveryAddress,
+        @NotNull(message = "주문 항목이 누락되었습니다.")
+        @NotEmpty(message = "주문 상품 목록은 비어있을 수 없습니다.")
         List<OrderItemInfo> orderItems
 ) {
 }
