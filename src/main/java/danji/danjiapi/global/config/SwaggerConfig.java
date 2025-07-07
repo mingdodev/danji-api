1199fd56f854bd3107845a3359c863f859d28fe6
mingdodev <mingdo.dev@gmail.com>
2025-07-08 01:08:11 +0900

    fix: Swagger 배포 경로 수정

--- src/main/java/danji/danjiapi/global/config/SwaggerConfig.java
+++ src/main/java/danji/danjiapi/global/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -14,6 +15,10 @@ import org.springframework.context.annotation.Configuration;
                 description = "단지(단체 주문 지키미)의 비즈니스 로직에 대한 API 명세서입니다.",
                 version = "1.0"
         ),
+        servers = {
+                @Server(url = "https://danji.o-r.kr", description = "production"),
+                @Server(url = "http://localhost:8080", description = "develop")
+        },
         security = @SecurityRequirement(name = "bearerAuth")
 )
 @SecurityScheme(
