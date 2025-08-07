40a0ed45ee3f9ecbfbe8aaa146a80cb055bf80cc
mingdodev <mingdo.dev@gmail.com>
2025-08-07 19:46:19 +0900

    refactor: 중복 로직 분리 및 redis cache 제거
    - 템플릿 설정 및 생성 로직 분리
    - 캐시 값에 따라 ttl 달리 주기 위해 추상화 대신 redisTemplate 사용하는 것으로 변경
    
    #16

--- src/main/java/danji/danjiapi/global/config/RedisConfig.java
+++ src/main/java/danji/danjiapi/global/config/RedisConfig.java
@@ -1,16 +1,14 @@
 package danji.danjiapi.global.config;
 
-import java.time.Duration;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -22,12 +20,12 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-//    @Bean
-//    public LettuceConnectionFactory tokenRedisConnectionFactory() {
-//        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
-//        configuration.setDatabase(0);
-//        return new LettuceConnectionFactory(configuration);
-//    }
+    @Bean
+    public LettuceConnectionFactory tokenRedisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
+        configuration.setDatabase(0);
+        return new LettuceConnectionFactory(configuration);
+    }
 
     @Bean
     public LettuceConnectionFactory cacheRedisConnectionFactory() {
@@ -36,30 +34,29 @@ public class RedisConfig {
         return new LettuceConnectionFactory(configuration);
     }
 
-    @Bean
-    RedisCacheManager cacheManager() {
-        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer<>(Object.class)));
+//    @Bean
+//    @Qualifier("tokenRedisTemplate")
+//    RedisTemplate<String, Object> tokenRedisTemplate() {
+//        return redisTemplateResolver(tokenRedisConnectionFactory());
+//    }
 
-        return RedisCacheManager
-                .builder(cacheRedisConnectionFactory())
-                .cacheDefaults(cacheConfiguration)
-                .build();
+    @Bean
+    @Qualifier("cacheRedisTemplate")
+    RedisTemplate<String, Object> cacheRedisTemplate() {
+        return redisTemplateResolver(cacheRedisConnectionFactory());
     }
 
-//    @Bean
-//    RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(tokenRedisConnectionFactory());
-//
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-//
-//        Jackson2JsonRedisSerializer<Object> jsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-//        redisTemplate.setValueSerializer(jsonRedisSerializer);
-//        redisTemplate.setHashValueSerializer(jsonRedisSerializer);
-//
-//        return redisTemplate;
-//    }
+    private RedisTemplate<String, Object> redisTemplateResolver(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        Jackson2JsonRedisSerializer<Object> jsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        redisTemplate.setValueSerializer(jsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jsonRedisSerializer);
+
+        return redisTemplate;
+    }
 }
