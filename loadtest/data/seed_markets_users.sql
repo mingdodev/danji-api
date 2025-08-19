b43cc4eaf438ac3a219a1ac5716e12e01c00fc3b
mingdodev <mingdo.dev@gmail.com>
2025-08-20 00:59:54 +0900

    feat: 테스트 데이터 생성 쿼리 파일 작성
    - 100개일 때, 1,000개일 때, 100,000개일 때
    
    #18

---
+++ loadtest/data/seed_markets_users.sql
@@ -0,0 +1,21 @@
+-- @email은 테스트 데이터용 이메일 포맷
+-- generate_series 이용, 데이터 개수가 100개일 때, 1,000개일 때, 100,000개일 때
+
+WITH new_users AS (
+    INSERT INTO users (name, email, password, role, created_at)
+    SELECT
+        LEFT('user' || gs, 10) AS name,
+        ('u' || gs || '@email.com') AS email,
+        '1234',
+        'MERCHANT',
+        NOW() - (random() * 365 || ' days')::interval
+    FROM generate_series(1, 100) gs
+    RETURNING id
+)
+INSERT INTO markets (user_id, name, address, created_at)
+SELECT
+    id,
+    '가게 ' || id,
+    '서울특별시 서울로 ' || id || '번길',
+    NOW() - (random() * 365 || ' days')::interval
+FROM new_users;
