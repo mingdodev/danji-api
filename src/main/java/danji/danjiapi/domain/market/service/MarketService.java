be9af97b38e94176f1c88deb74e777ee25b930b3
mingdodev <mingdo.dev@gmail.com>
2025-08-22 21:33:07 +0900

    refactor: 중복 코드 제거 및 로그 레벨 변경
    #18

--- src/main/java/danji/danjiapi/domain/market/service/MarketService.java
+++ src/main/java/danji/danjiapi/domain/market/service/MarketService.java
@@ -27,6 +27,7 @@ public class MarketService {
     private final MarketRepository marketRepository;
     private final ProductRepository productRepository;
     private final CurrentUserResolver currentUserResolver;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     public MarketService(
             MarketRepository marketRepository,
@@ -39,9 +40,6 @@ public class MarketService {
         this.currentUserResolver = currentUserResolver;
         this.redisTemplate = redisTemplate;
     }
-
-    @Qualifier("cacheRedisTemplate")
-    private final RedisTemplate<String, Object> redisTemplate;
 
     public PaginationResponse<MarketDetail> searchMarkets(MarketSearchCondition searchCondition, Pageable pageable) {
         Slice<Market> markets;
@@ -93,7 +91,7 @@ public class MarketService {
         List<MarketDetail> cacheableMarkets = queriedMarkets.stream()
                 .map(MarketDetail::from).toList();
 
-        log.info("Setting cache for key: {}, with {} items.", cacheKey, cacheableMarkets.size());
+        log.debug("Setting cache for key: {}, with {} items.", cacheKey, cacheableMarkets.size());
         redisTemplate.opsForValue().set(cacheKey, cacheableMarkets, Duration.ofMinutes(10));
 
         int end = Math.min(start + pageable.getPageSize(), queriedMarkets.size());
