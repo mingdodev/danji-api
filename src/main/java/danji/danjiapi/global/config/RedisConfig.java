49a033ac720dc4d790c609e2e57ca119ef9f014f
mingdodev <mingdo.dev@gmail.com>
2025-08-27 22:43:05 +0900

    fix: 다형성 제거로 redis 역직렬화 오류 해결
    - 조회 데이터 타입은 늘 같으므로 일단 고정시켜 둠
    
    #20

--- src/main/java/danji/danjiapi/global/config/RedisConfig.java
+++ src/main/java/danji/danjiapi/global/config/RedisConfig.java
@@ -1,5 +1,10 @@
 package danji.danjiapi.global.config;
 
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import danji.danjiapi.domain.market.dto.response.MarketDetail;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +14,9 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.yaml.snakeyaml.error.Mark;
 
 @Configuration
 public class RedisConfig {
@@ -20,13 +27,13 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Bean
-    @Qualifier("tokenRedisConnectionFactory")
-    public LettuceConnectionFactory tokenRedisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
-        configuration.setDatabase(0);
-        return new LettuceConnectionFactory(configuration);
-    }
+//    @Bean
+//    @Qualifier("tokenRedisConnectionFactory")
+//    public LettuceConnectionFactory tokenRedisConnectionFactory() {
+//        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
+//        configuration.setDatabase(0);
+//        return new LettuceConnectionFactory(configuration);
+//    }
 
     @Bean
     @Qualifier("cacheRedisConnectionFactory")
@@ -36,31 +43,51 @@ public class RedisConfig {
         return new LettuceConnectionFactory(configuration);
     }
 
-    @Bean
-    @Qualifier("tokenRedisTemplate")
-    RedisTemplate<String, Object> tokenRedisTemplate(
-            @Qualifier("tokenRedisConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
-        return redisTemplateResolver(redisConnectionFactory);
-    }
+//    @Bean
+//    @Qualifier("tokenRedisTemplate")
+//    RedisTemplate<String, Object> tokenRedisTemplate(
+//            @Qualifier("tokenRedisConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
+//        return redisTemplateResolver(redisConnectionFactory);
+//    }
 
     @Bean
     @Qualifier("cacheRedisTemplate")
-    RedisTemplate<String, Object> cacheRedisTemplate(
+    RedisTemplate<String, List<MarketDetail>> cacheRedisTemplate(
             @Qualifier("cacheRedisConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
-        return redisTemplateResolver(redisConnectionFactory);
-    }
+        ObjectMapper objectMapper = new ObjectMapper()
+                .findAndRegisterModules()
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-    private RedisTemplate<String, Object> redisTemplateResolver(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
+        JavaType listType = objectMapper.getTypeFactory()
+                .constructCollectionType(List.class, MarketDetail.class);
+
+        Jackson2JsonRedisSerializer<List<MarketDetail>> serializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, listType);
+
+        RedisTemplate<String, List<MarketDetail>> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
 
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(serializer);
+        redisTemplate.setHashValueSerializer(serializer);
 
-        GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
-        redisTemplate.setValueSerializer(jsonRedisSerializer);
-        redisTemplate.setHashValueSerializer(jsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
 
         return redisTemplate;
     }
+
+//    private RedisTemplate<String, Object> redisTemplateResolver(RedisConnectionFactory connectionFactory) {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(connectionFactory);
+//
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+//
+//        GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+//        redisTemplate.setValueSerializer(jsonRedisSerializer);
+//        redisTemplate.setHashValueSerializer(jsonRedisSerializer);
+//
+//        return redisTemplate;
+//    }
 }
