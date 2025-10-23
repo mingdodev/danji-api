8ed6c10e32baa2f6ea5be0ce27bf892981bc2202
mingdodev <mingdo.dev@gmail.com>
2025-10-23 19:11:13 +0900

    feat: ArchUnit 의존성 추가 및 테스트 작성
    #23

---
+++ src/test/java/danji/danjiapi/ArchitectureTest.java
@@ -0,0 +1,25 @@
+package danji.danjiapi;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.library.Architectures;
+
+@AnalyzeClasses(packages = "danji.danjiapi")
+public class ArchitectureTest {
+    @ArchTest
+    void layeredArchitectureTest(JavaClasses classes) {
+        Architectures.layeredArchitecture()
+                .consideringAllDependencies()
+                .layer("presentation").definedBy("..controller..", "..dto..")
+                .layer("application").definedBy("..service..", "..entity..")
+                .layer("persistence").definedBy("..repository..")
+
+                .whereLayer("presentation").mayNotBeAccessedByAnyLayer()
+                .whereLayer("application").mayOnlyBeAccessedByLayers("presentation")
+                .whereLayer("persistence").mayOnlyBeAccessedByLayers("application")
+
+                .check(classes);
+    }
+}
+
