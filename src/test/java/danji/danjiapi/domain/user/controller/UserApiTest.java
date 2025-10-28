4d9c7b9f371e06b6e5c3b9437c6ba0b29aa7558f
mingdodev <mingdo.dev@gmail.com>
2025-10-28 23:19:03 +0900

    test: UserApiTest 생성 및 고객 회원 가입 API 테스트 작성
    - 테스트 의존성 주입 환경 설정 추가
    
    #25

---
+++ src/test/java/danji/danjiapi/domain/user/controller/UserApiTest.java
@@ -0,0 +1,63 @@
+package danji.danjiapi.domain.user.controller;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.notNull;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import danji.danjiapi.domain.user.dto.request.UserCreateCustomerRequest;
+import danji.danjiapi.domain.user.dto.response.UserCreateCustomerResponse;
+import danji.danjiapi.domain.user.entity.Role;
+import danji.danjiapi.domain.user.entity.User;
+import danji.danjiapi.domain.user.repository.UserRepository;
+import danji.danjiapi.global.response.ApiResponse;
+import jakarta.transaction.Transactional;
+import java.io.UnsupportedEncodingException;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.assertj.MvcTestResult;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@RequiredArgsConstructor
+class UserApiTest {
+    final MockMvcTester mvcTester;
+    final ObjectMapper objectMapper;
+    final UserRepository userRepository;
+
+    @Test
+    void signupCustomer() throws JsonProcessingException, UnsupportedEncodingException {
+        var request = new UserCreateCustomerRequest("email@example.com", "1234", "tester");
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        MvcTestResult result = mvcTester.post().uri("/api/users/signup/customer").contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson).exchange();
+
+        assertThat(result)
+                .hasStatusOk();
+
+        String responseJson = result.getResponse().getContentAsString();
+
+        var apiResponse = objectMapper.readValue(responseJson,
+                new TypeReference<ApiResponse<UserCreateCustomerResponse>>() {});
+
+        UserCreateCustomerResponse response = apiResponse.data();
+        assertNotNull(response);
+        assertThat(response.id()).isNotNull();
+
+        User user = userRepository.findById(response.id()).orElseThrow();
+        assertThat(user.getEmail()).isEqualTo(request.email());
+        assertThat(user.getRole()).isEqualTo(Role.CUSTOMER);
+    }
+
+
+}
