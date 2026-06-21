131b3c1330a39f2cc21adea06473ff52be8a4124
mingdodev <mingdo.dev@gmail.com>
2026-06-21 17:18:56 +0900

    refactor: Redis 캐싱 코드 및 의존성 전면 제거
    - MarketService RedisTemplate 주입 제거, RedisConfig 파일 삭제
    - build.gradle 의존성 및 application.yml Redis 설정 제거
    - docker-compose api/chat valkey 의존성 제거, valkey 서비스 주석 처리
    
    #27

--- src/main/java/danji/danjiapi/domain/market/service/MarketService.java
+++ src/main/java/danji/danjiapi/domain/market/service/MarketService.java
@@ -1,44 +1,28 @@
 package danji.danjiapi.domain.market.service;
 
 import danji.danjiapi.domain.market.dto.request.MarketSearchCondition;
-import danji.danjiapi.domain.market.dto.response.MarketDetail;
 import danji.danjiapi.domain.market.entity.Market;
 import danji.danjiapi.domain.market.repository.MarketRepository;
 import danji.danjiapi.domain.product.entity.Product;
 import danji.danjiapi.domain.product.repository.ProductRepository;
 import danji.danjiapi.global.exception.CustomException;
 import danji.danjiapi.global.exception.ErrorMessage;
-import danji.danjiapi.global.response.PaginationResponse;
 import danji.danjiapi.global.util.resolver.CurrentUserResolver;
 import danji.danjiapi.global.util.validator.AccessValidator;
-import java.time.Duration;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class MarketService {
     private final MarketRepository marketRepository;
     private final ProductRepository productRepository;
     private final CurrentUserResolver currentUserResolver;
-    private final RedisTemplate<String, List<MarketDetail>> redisTemplate;
-
-    public MarketService(
-            MarketRepository marketRepository,
-            ProductRepository productRepository,
-            CurrentUserResolver currentUserResolver,
-            @Qualifier("cacheRedisTemplate") RedisTemplate<String, List<MarketDetail>> redisTemplate
-    ) {
-        this.marketRepository = marketRepository;
-        this.productRepository = productRepository;
-        this.currentUserResolver = currentUserResolver;
-        this.redisTemplate = redisTemplate;
-    }
 
     public Slice<Market> searchMarkets(MarketSearchCondition searchCondition, Pageable pageable) {
 
